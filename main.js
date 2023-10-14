e7daab8a2db6f8a9dfadda0f9da4ca60159b95f7
PatoVamp <toipiola123@gmail.com>
2023-10-14 17:50:28 -0500

    Proyecto Finalizado

--- main.js
+++ main.js
@@ -76,6 +76,23 @@ function openProductDetailAsideDesktop() {
     }
 
     productDetailContainerDesktop.classList.remove('inactive');
+
+    const productId = event.target.getAttribute('data-id');
+    const product = productList.find(p => p.id === productId);
+    
+    if (product) {
+        const imgProducts = document.getElementById('imgProduct');
+        imgProducts.setAttribute('src', product.image);
+
+        const infoProducts = document.getElementById('informationProduct');
+        infoProducts.textContent = product.infoproduct;
+
+        const nameProducts = document.getElementById('nameProduct');
+        nameProducts.textContent = product.name;
+
+        const priceProducts = document.getElementById('priceProduct');
+        priceProducts.textContent = product.price;
+    }
 }
 function closeProductDetailAsideDesktop() {
     
@@ -121,68 +138,109 @@ function toggleProductDetailAsideResponsive() {
     cartMenu.classList.toggle('inactive');
 
     productDetailContainerResponsive.classList.toggle('inactive');
+
+    const productId = event.target.getAttribute('data-id');
+    const product = productList.find(p => p.id === productId);
+    
+    if (product) {
+        const imgProducts = document.getElementById('imgProduct');
+        imgProducts.setAttribute('src', product.image);
+
+        const infoProducts = document.getElementById('informationProduct');
+        infoProducts.textContent = product.infoproduct;
+
+        const nameProducts = document.getElementById('nameProduct');
+        nameProducts.textContent = product.name;
+
+        const priceProducts = document.getElementById('priceProduct');
+        priceProducts.textContent = product.price;
+    }
 }
 
 const productList = []
 productList.push({
+    id: "producto1",
     name: "Bike",
-    price: 120,
+    price: "$120",
     image: "https://media.istockphoto.com/id/482965873/es/foto/bicicleta-de-monta%C3%B1a-de-29-pulgadas-con-ruedas-sobre-blanco.jpg?s=1024x1024&w=is&k=20&c=LPHFQ8-e0KIDRBS1AdztC7kKZon-GMedhevjM-CZ7_M=",
+    infoproduct: "RoadMaster Hurricane 29 Stainless Steel Frame, with YS Code paint Fork: RoadMaster Steel 28.6 * 25.4* with Suspension Lockout Suspension with Shoulder Lock Handlebar: RoadMaster Swallow 22.2*640MM"
 });
 productList.push({
+    id: "producto2",
     name: "Casual Shoes ",
-    price: 40,
+    price: "$40",
     image: "https://http2.mlstatic.com/D_NQ_NP_2X_961962-MCO53969482763_022023-F.webp",
+    infoproduct: "Our products are manufactured for everyday use with high quality and at an affordable price for the entire community. We seek to impact our customers, guided by the quality and comfort of our products to generate a constant, long-term relationship and on the best terms."
 });
 productList.push({
+    id: "producto3",
     name: "Computer",
-    price: 635,
+    price: "$635",
     image: "https://http2.mlstatic.com/D_NQ_NP_962770-MLM54029806942_022023-O.webp",
+    infoproduct: "Powered by the Intel Core processor, the Victus by HP 16.1-inch laptop has all the features to handle your gaming and daily needs. Increase your gaming flexibility with a multi-use gaming keyboard and enjoy a fast refresh rate display without image defects."
 });
 productList.push({
+    id: "producto4",
     name: "Chair",
-    price: 45,
+    price: "$45",
     image: "https://http2.mlstatic.com/D_NQ_NP_2X_699538-MCO69510888045_052023-F.webp",
+    infoproduct: "Remember that to issue the electronic invoice you have a maximum period of 10 calendar days after receiving the purchase to request it. You must send us the following information: name/company name, CC/NIT, address and email."
 });
 productList.push({
+    id: "producto5",
     name: "Desktop Gamer",
-    price: 130,
+    price: "$130",
     image: "https://http2.mlstatic.com/D_NQ_NP_2X_924794-MLA54287479221_032023-F.webp",
+    infoproduct: "Program your play area! Resistant and spacious, the Madesa desk is perfect for those looking for a spacious piece of furniture that can support the weight of consoles, televisions, large screens and other devices required for playing long hours."
 });
 productList.push({
+    id: "producto6",
     name: "Diapers",
-    price: 50,
+    price: "$50",
     image: "https://jumbocolombiaio.vtexassets.com/arquivos/ids/184673-1600-1600?v=637813974873170000&width=1600&height=1600&aspect=true",
+    infoproduct: "Like most modern disposable diapers, Pampers diapers have a layer of materials that allows the transfer and distribution of liquid to an absorbent center where the liquid is away from the baby, keeping him dry and comfortable."
 });
 productList.push({
+    id: "producto7",
     name: "Electric Stove",
-    price: 110,
+    price: "$110",
     image: "https://http2.mlstatic.com/D_NQ_NP_734749-MLU71092971649_082023-O.webp",
+    infoproduct: "This Challenger stove will help you show off and surprise all your guests. Its design and functionality guarantee the best performance so that you can prepare your dishes in a comfortable and practical way."
 });
 productList.push({
+    id: "producto8",
     name: "Mistery Box",
-    price: 50,
+    price: "$50",
     image: "https://http2.mlstatic.com/D_NQ_NP_2X_861151-MCO52815488323_122022-F.webp",
+    infoproduct: "Mystery box that offers you the opportunity at a low price to obtain watch products; jewelery (necklaces, bracelets, rings) of this price range, take advantage of this opportunity to take this box containing discounted products"
 });
 productList.push({
+    id: "producto9",
     name: "Sports Clothing",
-    price: 40,
+    price: "$40",
     image: "https://http2.mlstatic.com/D_NQ_NP_847095-MCO51526024647_092022-O.webp",
+    infoproduct: "JOGGER + SWIMSUIT set in brushed cotton to be used on all occasions, casual or as sports, very comfortable and resistant, does not become speckled, top quality fabric."
 });
 productList.push({
+    id: "producto10",
     name: "Toys",
-    price: 25,
+    price: "$25",
     image: "https://http2.mlstatic.com/D_NQ_NP_2X_739740-MCO47849335494_102021-F.webp",
+    infoproduct: "With hobby toys you can let your imagination fly with the most fun and magical creations. You will learn by playing, explore your creative side and put your motor and memory skills into practice. Are you ready to become an architect of your own adventures?"
 });
 productList.push({
+    id: "producto11",
     name: "Weight Kit",
-    price: 25,
+    price: "$25",
     image: "https://http2.mlstatic.com/D_NQ_NP_772995-MCO52821182277_122022-O.webp",
+    infoproduct: "Muscles are strengthened and simultaneously shaped with the use of weights. Therefore, weight training not only has a sporting effect but also an aesthetic effect."
 });
 productList.push({
+    id: "producto12",
     name: "Xbox One S",
-    price: 420,
+    price: "$420",
     image: "https://http2.mlstatic.com/D_NQ_NP_2X_641295-MLA69573719686_052023-F.webp",
+    infoproduct: "The new generation of consoles is led by the Xbox Series that came to the market to surprise everyone. Its power and high performance will allow you to reduce the hours of downloading games and content considerably compared to other consoles."
 });
 
 
@@ -193,6 +251,7 @@ function renderProductsResponsive(arr) {
 
         const productImgResponsive = document.createElement('img');
         productImgResponsive.setAttribute('src', product.image);
+        productImgResponsive.setAttribute('data-id', product.id);
         productImgResponsive.addEventListener('click', toggleProductDetailAsideResponsive);
 
         const productInfoResponsive = document.createElement('div');
@@ -201,7 +260,7 @@ function renderProductsResponsive(arr) {
         const productInfoDivResponsive = document.createElement('div');
 
         const productPrice = document.createElement('p');
-        productPrice.innerText = '$' + product.price;
+        productPrice.innerText = product.price;
     
         const productName = document.createElement('p');
         productName.innerText = product.name;
@@ -233,6 +292,7 @@ function renderProductsDesktop(arr) {
         const productImgDesktop = document.createElement('img');
         productImgDesktop.setAttribute('src', product.image);
         productImgDesktop.addEventListener('click', openProductDetailAsideDesktop);
+        productImgDesktop.setAttribute('data-id', product.id);
 
         const productInfoDesktop = document.createElement('div');
         productInfoDesktop.classList.add('product-info-desktop');
@@ -240,7 +300,7 @@ function renderProductsDesktop(arr) {
         const productInfoDivDesktop = document.createElement('div');
 
         const productPrice = document.createElement('p');
-        productPrice.innerText = '$' + product.price;
+        productPrice.innerText = product.price;
     
         const productName = document.createElement('p');
         productName.innerText = product.name;
@@ -266,3 +326,27 @@ function renderProductsDesktop(arr) {
 
 renderProductsDesktop(productList);
 renderProductsResponsive(productList);
+
+function insertContentInfo() {
+    productList.forEach(product => {
+        const imgProducts = document.getElementById('imgProduct');
+        imgProducts.setAttribute('src', product.image);
+    });
+    
+    productList.forEach(product => {
+        const infoProducts = document.getElementById('informationProduct');
+        infoProducts.textContent = product.infoproduct
+    });
+    
+    productList.forEach(product => {
+        const nameProducts = document.getElementById('nameProduct');
+        nameProducts.textContent = product.name
+    });
+    
+    productList.forEach(product => {
+        const priceProducts = document.getElementById('priceProduct');
+        priceProducts.textContent = product.price
+    });
+}
+
+insertContentInfo()
